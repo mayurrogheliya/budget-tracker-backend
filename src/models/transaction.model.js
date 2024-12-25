cc6a9b9fa15c5ee093e979b19cebabea8a50a935
Mayur Rogheliya <mayurrogheliya@gmail.com>
2024-12-25 23:08:13 +0530

    feat: transaction model is created

---
+++ src/models/transaction.model.js
@@ -0,0 +1,13 @@
+import mongoose, { Schema } from "mongoose"
+
+const transactionSchema = new Schema(
+    {
+        type: { type: String, required: false },
+        amount: { type: Number, required: false },
+        description: { type: String, required: false },
+        date: { type: Date, required: false },
+    },
+    { timestamps: true }
+)
+
+export const Transaction = mongoose.model("Transaction", transactionSchema);
