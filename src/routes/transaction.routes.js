7d3d2c0bb261d9065e3a057b214e2155c863f395
Mayur Rogheliya <mayurrogheliya@gmail.com>
2024-12-28 00:49:07 +0530

    feat: add analytics routes for analysis of income and expense

--- src/routes/transaction.routes.js
+++ src/routes/transaction.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createTransaction, deleteTransaction, getAllTransactions, updateTransaction } from "../controllers/transaction.controller.js";
+import { createTransaction, deleteTransaction, getAllTransactions, getAnalytics, updateTransaction } from "../controllers/transaction.controller.js";
 
 const router = Router();
 
@@ -7,5 +7,6 @@ router.get("/", getAllTransactions);
 router.post("/", createTransaction);
 router.put("/:_id", updateTransaction);
 router.delete("/:_id", deleteTransaction);
+router.get("/list", getAnalytics);
 
 export default router;
