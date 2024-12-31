4fcd8d6f7c9edc3b22ba297eb6a73af61a8903f0
Mayur Rogheliya <mayurrogheliya@gmail.com>
2024-12-31 17:52:52 +0530

    implement an auth midldreware for user authentication and token verification

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,38 @@
+import jwt from "jsonwebtoken";
+import ResponseData from "../utils/ApiResponse.js";
+import asyncHandler from "../utils/asyncHandler.js";
+
+const authenticateUser = asyncHandler(async (req, res, next) => {
+    try {
+        const token = await req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
+        console.log(token);
+
+        if (!token) {
+            return ResponseData(res, {
+                statusCode: 401,
+                status: "error",
+                message: "Unauthorized, token not provided",
+            });
+        }
+
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+            if (err) {
+                return ResponseData(res, {
+                    statusCode: 401,
+                    status: "error",
+                    message: "Invalid token",
+                });
+            }
+            req.user = user;
+            next();
+        })
+    } catch (error) {
+        return ResponseData(res, {
+            statusCode: 500,
+            status: "error",
+            message: "Internal server error",
+        });
+    }
+});
+
+export default authenticateUser;
