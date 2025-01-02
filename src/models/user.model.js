be10e60e3dd7d09a0647703d5b03f5abd6a20380
Mayur Rogheliya <mayurrogheliya@gmail.com>
2025-01-02 08:58:29 +0530

    add accessToken field in user schema

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -22,6 +22,9 @@ const userSchema = new Schema(
         },
         refreshToken: {
             type: String,
+        },
+        accessToken: {
+            type: String,
         }
     },
     { timestamps: true }
