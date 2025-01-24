1693bdb5d22c78fdaa7762b0b97f57db23e3e807
Mayur Rogheliya <mayurrogheliya@gmail.com>
2025-01-24 19:02:42 +0530

    add resetToken method to User schema

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -37,6 +37,7 @@ const userSchema = new Schema(
 userSchema.pre('save', async function (next) {
     if (!this.isModified("password")) return next();
     this.password = await bcrypt.hash(this.password, 10);
+    next();
 })
 
 userSchema.methods.verifyPassword = async function (password) {
@@ -53,6 +54,10 @@ userSchema.methods.generateRefreshToken = async function () {
 
 userSchema.methods.generateVerificationToken = async function () {
     return jwt.sign({ _id: this._id, email: this.email }, process.env.VERIFICATION_TOKEN_SECRET, { expiresIn: process.env.VERIFICATION_TOKEN_EXPIRY })
+}
+
+userSchema.methods.resetToken = async function () {
+    return jwt.sign({ _id: this._id }, process.env.RESET_TOKEN_SECRET, { expiresIn: process.env.RESET_TOKEN_EXPIRY })
 }
 
 export const User = mongoose.model("User", userSchema);
