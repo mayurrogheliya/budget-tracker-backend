e00d11bd90cb2dd8c045bee5d09330003840e02c
Mayur Rogheliya <mayurrogheliya@gmail.com>
2025-01-24 19:01:15 +0530

    implement forgot and reset password functionality

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -30,6 +30,7 @@ export const createUser = asyncHandler(async (req, res) => {
         <h1>Email Verification</h1>
         <p>Thank you for registering. Please verify your email by clicking the link below:</p>
         <a href="${verificationLink}" target="_blank">Verify Email</a>
+        <p>If you didn't sign up for the Budget Tracker app, please ignore this email. Otherwise, you can continue using your email address to log in.</p>
         <p>This link is valid for 24 hours.</p>
     `;
 
@@ -171,6 +172,73 @@ export const verifyEmail = asyncHandler(async (req, res) => {
         return ResponseData(res, {
             statusCode: 200,
             message: "Email verified successfully",
+        });
+    } catch (error) {
+        return ResponseData(res, {
+            statusCode: 400,
+            message: "Invalid or expired token",
+        });
+    }
+})
+
+export const forgotPassword = asyncHandler(async (req, res) => {
+    const { email } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return ResponseData(res, {
+                statusCode: 400,
+                message: "No account found with that email address.",
+            });
+        }
+
+        const resetToken = await user.resetToken();
+
+        const resetURL = `${process.env.USER_URL}/reset-password/${resetToken}`;
+
+        const subject = "Password Reset - Budget Tracker App";
+        const message = `
+            <h1>Password Reset</h1>
+            <p>You requested to reset your password. Please click the link below to reset it:</p>
+            <a href="${resetURL}" target="_blank">Reset Password</a>
+            <p>If you did not request a password reset, please ignore this email.</p>
+            <p>This link is valid for 15 minutes.</p>
+        `;
+
+        await sendEmail(user.email, subject, message);
+        return ResponseData(res, {
+            statusCode: 200,
+            message: "Please check your email to reset your password.",
+        });
+    } catch (error) {
+        return ResponseData(res, {
+            statusCode: 500,
+            message: "Internal server error",
+        });
+    }
+})
+
+export const resetPassword = asyncHandler(async (req, res) => {
+    const { token } = req.params;
+    const { password } = req.body;
+
+    try {
+        const decode = jwt.verify(token, process.env.RESET_TOKEN_SECRET);
+
+        const user = await User.findById(decode._id);
+        if (!user) {
+            return ResponseData(res, {
+                statusCode: 400,
+                message: "No account found with that email address.",
+            });
+        }
+
+        user.password = password;
+        await user.save();
+
+        return ResponseData(res, {
+            statusCode: 200,
+            message: "Password reset successfully",
         });
     } catch (error) {
         return ResponseData(res, {
